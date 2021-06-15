5a7ea6aaba5740d5c407dfb883b0254c3ec554af
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-06-15 14:32:48 +0300

    Add Stack class members

--- src/Stack/Stack.cs
+++ src/Stack/Stack.cs
@@ -1,7 +1,48 @@
+using System.Collections.Generic;
 namespace Collections
 {
     public class Stack<T>
     {
+        private LinkedList<T> _dataList;
 
+        public int Size
+        {
+            get
+            {
+                return _dataList.Count;
+            }
+        }
+
+        public Stack()
+        {
+        }
+
+        public bool Push(T newElement)
+        {
+            //TODO: implement
+            return false;
+        }
+
+        public T Pop()
+        {
+            //TODO: implement
+            return default(T);
+        }
+
+        public void Print()
+        {
+            //TODO: implement
+        }
+
+        public T Peak()
+        {
+            //TODO: implement
+            return default(T);
+        }
+
+        public void Clear()
+        {
+            //TODO: implement
+        }
     }
 }
